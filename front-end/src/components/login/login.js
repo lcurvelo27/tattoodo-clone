12b28c60f9050f41886c5aa5d42350a191226415
Lcurvelo27 <lcurvelo27@gmail.com>
2017-12-03 11:30:03 -0700

    fixed margin issues from root component

--- front-end/src/components/login/login.js
+++ front-end/src/components/login/login.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import '../../normalize.css'
 import './login.css';
 
 class Login extends Component {
   render() {
     return (
-      <div className="login-container">
+      <div className="container">
       	<h1>Get Blasted</h1>
         <a href='http://localhost:3000/auth'><button>Login/Register</button></a>
       </div>
